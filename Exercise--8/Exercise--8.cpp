f5488af15bf60bf2dc9377bc39d58e7060f0597c
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 03:23:43 +0000

    Add project files.

---
+++ Exercise--8/Exercise--8.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+        int n;
+        scanf_s("%d", &n);
+        int number;
+        int min = INT_MAX; 
+        for (int i = 0; i < n; i++) {
+            printf("Enter number %d: ", i + 1);
+            scanf_s("%d", &number);
+
+            
+            if (number < min) {
+                min = number; 
+            }
+        }
+        printf("The smallest number is: %d", min);
+}
